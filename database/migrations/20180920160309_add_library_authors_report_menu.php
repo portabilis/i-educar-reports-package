bf48c9c23611470f8c3c11a0f1e327d9151694d3
Eder Soares <edersoares@me.com>
2018-09-20 14:36:13 -0300

    Relatório de autores

---
+++ database/migrations/20180920160309_add_library_authors_report_menu.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddLibraryAuthorsReportMenu extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute(
+            '
+                INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, title, nivel) 
+                VALUES (999615, 57, 2, \'Relatório de autores\', \'module/Reports/LibraryAuthors\', null, 3);
+                
+                INSERT INTO pmicontrolesis.menu (cod_menu, ref_cod_menu_submenu, ref_cod_menu_pai, tt_menu, ord_menu, caminho, alvo, suprime_menu, ref_cod_tutormenu, ref_cod_ico, tipo_menu) 
+                VALUES (999615, 999615, 999905, \'Relatório de autores\', 1, \'module/Reports/LibraryAuthors\', \'_self\', 1, 16, 1, null);
+                
+                INSERT INTO pmieducar.menu_tipo_usuario (ref_cod_tipo_usuario, ref_cod_menu_submenu, cadastra, visualiza, exclui) 
+                VALUES (1, 999615, 1, 0, 1);
+                
+                INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) 
+                VALUES (1, 0, 0, 999615);
+            '
+        );
+    }
+
+    public function down()
+    {
+        $this->execute(
+            '
+
+                DELETE FROM portal.menu_funcionario 
+                WHERE ref_cod_menu_submenu = 999615;
+                
+                DELETE FROM pmieducar.menu_tipo_usuario 
+                WHERE ref_cod_menu_submenu = 999615;
+
+                DELETE FROM pmicontrolesis.menu 
+                WHERE cod_menu = 999615;
+
+                DELETE FROM portal.menu_submenu 
+                WHERE cod_menu_submenu = 999615;
+            '
+        );
+    }
+}
