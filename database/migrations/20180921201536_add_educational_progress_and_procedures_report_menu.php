78d1c038cee47da236974a88c1acdfdd5d5d342e
Eder Soares <edersoares@me.com>
2018-09-21 17:28:42 -0300

    Relatório de rendimento e movimento escolar

---
+++ database/migrations/20180921201536_add_educational_progress_and_procedures_report_menu.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddEducationalProgressAndProceduresReportMenu extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute(
+            '
+                INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, title, nivel) 
+                VALUES (999830, 55, 2, \'Rendimento e movimento escolar\', \'module/Reports/EducationalProgressAndProcedures\', null, 3);
+                
+                INSERT INTO pmicontrolesis.menu (cod_menu, ref_cod_menu_submenu, ref_cod_menu_pai, tt_menu, ord_menu, caminho, alvo, suprime_menu, ref_cod_tutormenu, ref_cod_ico, tipo_menu) 
+                VALUES (999830, 999830, 999303, \'Rendimento e movimento escolar\', 0, \'module/Reports/EducationalProgressAndProcedures\', \'_self\', 1, 15, 192, null);
+                
+                INSERT INTO pmieducar.menu_tipo_usuario (ref_cod_tipo_usuario, ref_cod_menu_submenu, cadastra, visualiza, exclui) 
+                VALUES (1, 999830, 1, 0, 1);
+            '
+        );
+    }
+
+    public function down()
+    {
+        $this->execute(
+            '
+                DELETE FROM pmieducar.menu_tipo_usuario 
+                WHERE ref_cod_menu_submenu = 999830;
+
+                DELETE FROM pmicontrolesis.menu 
+                WHERE cod_menu = 999830;
+
+                DELETE FROM portal.menu_submenu 
+                WHERE cod_menu_submenu = 999830;
+            '
+        );
+    }
+}
