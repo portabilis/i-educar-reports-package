15dc9f8bb33158221b14cb45a0a7544834938f3b
Eder Soares <edersoares@me.com>
2018-09-21 14:38:32 -0300

    Relação de docentes e disciplinas lecionadas por turma

---
+++ database/migrations/20180921173535_add_teachers_and_courses_taught_by_class_report_menu.php
@@ -0,0 +1,44 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddTeachersAndCoursesTaughtByClassReportMenu extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute(
+            '
+                INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, title, nivel) 
+                VALUES (999860, 71, 2, \'Relatório de docentes e disciplinas lecionadas por turma\', \'module/Reports/TeachersAndCoursesTaughtByClass\', null, 3);
+                
+                INSERT INTO pmicontrolesis.menu (cod_menu, ref_cod_menu_submenu, ref_cod_menu_pai, tt_menu, ord_menu, caminho, alvo, suprime_menu, ref_cod_tutormenu, ref_cod_ico, tipo_menu) 
+                VALUES (999860, 999860, 999914, \'Relatório de docentes e disciplinas lecionadas por turma\', 0, \'module/Reports/TeachersAndCoursesTaughtByClass\', \'_self\', 1, 19, 192, 1);
+                
+                INSERT INTO pmieducar.menu_tipo_usuario (ref_cod_tipo_usuario, ref_cod_menu_submenu, cadastra, visualiza, exclui) 
+                VALUES (1, 999860, 1, 1, 1);
+                
+                INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) 
+                VALUES (1, 0, 0, 999860);
+            '
+        );
+    }
+
+    public function down()
+    {
+        $this->execute(
+            '
+                DELETE FROM portal.menu_funcionario 
+                WHERE ref_cod_menu_submenu = 999860;
+                
+                DELETE FROM pmieducar.menu_tipo_usuario 
+                WHERE ref_cod_menu_submenu = 999860;
+
+                DELETE FROM pmicontrolesis.menu 
+                WHERE cod_menu = 999860;
+
+                DELETE FROM portal.menu_submenu 
+                WHERE cod_menu_submenu = 999860;
+            '
+        );
+    }
+}
