a8a6f90747768aed6229ff7f3fd216d1a7bc1003
Eder Soares <edersoares@me.com>
2018-10-02 20:08:19 -0300

    Resultado final

---
+++ database/migrations/20181002224913_add_final_result_report_menu.php
@@ -0,0 +1,44 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddFinalResultReportMenu extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute(
+            '
+                INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, title, nivel) 
+                VALUES (999608, 55, 2, \'Resultado final\', \'module/Reports/FinalResult\', null, 3);
+                
+                INSERT INTO pmicontrolesis.menu (cod_menu, ref_cod_menu_submenu, ref_cod_menu_pai, tt_menu, ord_menu, caminho, alvo, suprime_menu, ref_cod_tutormenu, ref_cod_ico, tipo_menu) 
+                VALUES (999608, 999608, 999925, \'Resultado final\', 0, \'module/Reports/FinalResult\', \'_self\', 1, 15, 192, null);
+                
+                INSERT INTO pmieducar.menu_tipo_usuario (ref_cod_tipo_usuario, ref_cod_menu_submenu, cadastra, visualiza, exclui) 
+                VALUES (1, 999608, 1, 0, 1);
+                
+                INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) 
+                VALUES (1, 0, 0, 999608);
+            '
+        );
+    }
+
+    public function down()
+    {
+        $this->execute(
+            '
+                DELETE FROM portal.menu_funcionario 
+                WHERE ref_cod_menu_submenu = 999608;
+                
+                DELETE FROM pmieducar.menu_tipo_usuario 
+                WHERE ref_cod_menu_submenu = 999608;
+
+                DELETE FROM pmicontrolesis.menu 
+                WHERE cod_menu = 999608;
+
+                DELETE FROM portal.menu_submenu 
+                WHERE cod_menu_submenu = 999608;
+            '
+        );
+    }
+}
