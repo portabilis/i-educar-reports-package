d32a3114b301ecb2b1677a0d411e972f3bd7f125
Caroline Salib <carolinesalibc@gmail.com>
2019-01-29 11:43:27 -0200

    Adicionado script de criação de function para o boletim

--- database/sqls/initial-reports-database.sql
+++ database/sqls/initial-reports-database.sql
@@ -751,6 +751,38 @@ CREATE FUNCTION prioridade_historico(situacao numeric) RETURNS numeric
                         END;
                         $$;
 
+--
+-- Name: exibe_aluno_conforme_parametro_alunos_diferenciados(integer, integer); Type: FUNCTION; Schema: relatorio; Owner: ieducar
+--
+
+CREATE OR REPLACE FUNCTION relatorio.exibe_aluno_conforme_parametro_alunos_diferenciados(
+    codigo_aluno integer,
+    alunos_diferenciados integer)
+  RETURNS boolean AS
+$BODY$
+    DECLARE
+    possui_deficiencia boolean;
+    BEGIN
+
+    possui_deficiencia := EXISTS
+(SELECT 1
+       FROM cadastro.fisica_deficiencia fd
+       JOIN pmieducar.aluno a ON fd.ref_idpes = a.ref_idpes
+       JOIN cadastro.deficiencia d ON d.cod_deficiencia = fd.ref_cod_deficiencia
+      WHERE a.cod_aluno = codigo_aluno
+AND d.desconsidera_regra_diferenciada = false
+      LIMIT 1
+    );
+
+    CASE alunos_diferenciados
+        WHEN 1 THEN RETURN possui_deficiencia = false;
+        WHEN 2 THEN RETURN possui_deficiencia = true;
+        ELSE RETURN true;
+    END CASE;
+
+    END; $BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
 
 --
 -- Name: view_auditoria; Type: VIEW; Schema: relatorio; Owner: postgres
