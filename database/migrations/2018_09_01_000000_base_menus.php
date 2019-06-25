727b0c35f72abd8c22e9722f5b853d7c31436323
Eder Soares <edersoares@me.com>
2019-06-25 09:54:46 -0300

    Atualiza ao invés de duplicar

--- database/migrations/2018_09_01_000000_base_menus.php
+++ database/migrations/2018_09_01_000000_base_menus.php
@@ -13,166 +13,189 @@ class BaseMenus extends Migration
      */
     public function up()
     {
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 21126,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_SCHOOL)->firstOrFail()->getKey(),
             'title' => 'Relatórios',
             'order' => 5,
             'parent_old' => 15,
-            'old' => 21126,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 21127,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_SCHOOL)->firstOrFail()->getKey(),
             'title' => 'Documentos',
             'order' => 6,
             'parent_old' => 15,
-            'old' => 21127,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999301,
+        ], [
             'parent_id' => Menu::query()->where('old', 21126)->firstOrFail()->getKey(),
             'title' => 'Movimentações',
             'order' => 2,
             'parent_old' => 21126,
-            'old' => 999301,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999922,
+        ], [
             'parent_id' => Menu::query()->where('old', 21126)->firstOrFail()->getKey(),
             'title' => 'Lançamentos',
             'order' => 3,
             'parent_old' => 21126,
-            'old' => 999922,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999300,
+        ], [
             'parent_id' => Menu::query()->where('old', 21126)->firstOrFail()->getKey(),
             'title' => 'Cadastrais',
             'order' => 4,
             'parent_old' => 21126,
-            'old' => 999300,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999923,
+        ], [
             'parent_id' => Menu::query()->where('old', 21126)->firstOrFail()->getKey(),
             'title' => 'Matrículas',
             'order' => 5,
             'parent_old' => 21126,
-            'old' => 999923,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999303,
+        ], [
             'parent_id' => Menu::query()->where('old', 21126)->firstOrFail()->getKey(),
             'title' => 'Indicadores',
             'order' => 6,
             'parent_old' => 21126,
-            'old' => 999303,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999400,
+        ], [
             'parent_id' => Menu::query()->where('old', 21127)->firstOrFail()->getKey(),
             'title' => 'Atestados',
             'order' => 0,
             'parent_old' => 21127,
-            'old' => 999400,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999450,
+        ], [
             'parent_id' => Menu::query()->where('old', 21127)->firstOrFail()->getKey(),
             'title' => 'Boletins',
             'order' => 2,
             'parent_old' => 21127,
-            'old' => 999450,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999925,
+        ], [
             'parent_id' => Menu::query()->where('old', 21127)->firstOrFail()->getKey(),
             'title' => 'Resultados',
             'order' => 3,
             'parent_old' => 21127,
-            'old' => 999925,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999861,
+        ], [
             'parent_id' => Menu::query()->where('old', 21127)->firstOrFail()->getKey(),
             'title' => 'Fichas',
             'order' => 8,
             'parent_old' => 21127,
-            'old' => 999861,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999460,
+        ], [
             'parent_id' => Menu::query()->where('old', 21127)->firstOrFail()->getKey(),
             'title' => 'Históricos',
             'order' => 9,
             'parent_old' => 21127,
-            'old' => 999460,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999500,
+        ], [
             'parent_id' => Menu::query()->where('old', 21127)->firstOrFail()->getKey(),
             'title' => 'Registros',
             'order' => 10,
             'parent_old' => 21127,
-            'old' => 999500,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999614,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_LIBRARY)->firstOrFail()->getKey(),
             'title' => 'Relatórios',
             'order' => 3,
             'parent_old' => 16,
-            'old' => 999614,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999831,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_LIBRARY)->firstOrFail()->getKey(),
             'title' => 'Documentos',
             'order' => 4,
             'parent_old' => 16,
-            'old' => 999831,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999905,
+        ], [
             'parent_id' => Menu::query()->where('old', 999614)->firstOrFail()->getKey(),
             'title' => 'Cadastrais',
             'order' => 1,
             'parent_old' => 999614,
-            'old' => 999905,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999906,
+        ], [
             'parent_id' => Menu::query()->where('old', 999614)->firstOrFail()->getKey(),
             'title' => 'Movimentações',
             'order' => 2,
             'parent_old' => 999614,
-            'old' => 999906,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999907,
+        ], [
             'parent_id' => Menu::query()->where('old', 999831)->firstOrFail()->getKey(),
             'title' => 'Comprovantes',
             'order' => 3,
             'parent_old' => 999831,
-            'old' => 999907,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 20712,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_TRANSPORT)->firstOrFail()->getKey(),
             'title' => 'Relatórios',
             'order' => 3,
             'parent_old' => 17,
-            'old' => 20712,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 9998847,
+        ], [
             'parent_id' => Menu::query()->where('old', 20712)->firstOrFail()->getKey(),
             'title' => 'Cadastrais',
             'order' => 1,
             'parent_old' => 20712,
-            'old' => 9998847,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999913,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_EMPLOYEES)->firstOrFail()->getKey(),
             'title' => 'Relatórios',
             'order' => 2,
             'parent_old' => 71,
-            'old' => 999913,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999916,
+        ], [
             'parent_id' => Menu::query()->where('old', Process::MENU_EMPLOYEES)->firstOrFail()->getKey(),
             'title' => 'Documentos',
             'order' => 3,
             'parent_old' => 71,
-            'old' => 999916,
         ]);
-        Menu::query()->create([
+        Menu::query()->updateOrCreate([
+            'old' => 999914,
+        ], [
             'parent_id' => Menu::query()->where('old', 999913)->firstOrFail()->getKey(),
             'title' => 'Cadastrais',
             'order' => 1,
             'parent_old' => 999913,
-            'old' => 999914,
         ]);
     }
 
