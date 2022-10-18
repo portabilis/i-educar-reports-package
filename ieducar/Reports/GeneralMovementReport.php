09a9c98e3be0733158c913e9e6a9401e225ccfa9
bonot <paulabonot@gmail.com>
2022-10-18 10:59:21 -0300

    Cria relatório de movimento geral

---
+++ ieducar/Reports/GeneralMovementReport.php
@@ -0,0 +1,30 @@
+<?php
+
+use iEducar\Reports\JsonDataSource;
+
+class GeneralMovementReport extends Portabilis_Report_ReportCore
+{
+    use JsonDataSource, GeneralMovementTrait {
+        GeneralMovementTrait::query as QueryGeneralMovement;
+    }
+
+    public function templateName()
+    {
+        return 'general-movement';
+    }
+
+    public function requiredArgs()
+    {
+        $this->addRequiredArg('ano');
+        $this->addRequiredArg('instituicao');
+    }
+
+    public function getJsonData()
+    {
+        return [
+            'main' => Portabilis_Utils_Database::fetchPreparedQuery($this->QueryGeneralMovement()),
+            'header' => Portabilis_Utils_Database::fetchPreparedQuery($this->getSqlHeaderReport())
+        ];
+    }
+
+}
